c3a7fa1a9374f5b754b2256d64ebb0e5516105c4
SukhmanSingh <sukhmansingh1603@gmail.com>
2020-04-05 22:07:50 -0700

    Initial Commit

---
+++ NoteManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteManager
+//
+//  Created by Sukhman Singh on 4/5/20.
+//  Copyright © 2020 Sukhman Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
